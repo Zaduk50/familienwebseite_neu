3a3901dbf44f8255fd1d1b81c52650db6e55fdfd
Zaduk50 <kame-sennin22@gmx.de>
2025-06-24 22:06:00 +0200

    gathered picture from Datamodel instead using the direct path

--- startseite/views.py
+++ startseite/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import Profile
 
 def startseite(request):
-    profiles = Profile.objects.all()
-    return render(request, 'startseite/startseite.html')
+    tristan_profile = Profile.objects.get(user__username='Tristan')
+    return render(request, 'startseite/startseite.html', {'tristan': tristan_profile})
 
 def familienbereich(request):
     return render(request, 'startseite/familienbereich.html')
